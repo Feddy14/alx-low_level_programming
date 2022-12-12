a3df97ab7b7096247429170ba9e3a6467e820925
Feddy14 <ikennaokafor220@gmail.com>
2022-12-12 00:51:30 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+/**
+ * main: entry point
+ * return: always 0
+ */
 
 int main(void)
 {
-	char str[] = "Programming is like building a multilingual puzzle";
-	puts(str);
+	char str[] = "\"Programming is like building a multilingual puzzle";
+
+	puts (str);
 return(0);
 }
